a4ad975599a7f6e3eee5fc416486f7956f121759
ajitesh70 <132501687+ajitesh70@users.noreply.github.com>
2026-04-09 17:43:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "Existing VPC Name"
+  type        = string
+  default     = "OTMS-vpc"
+}
