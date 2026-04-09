49d332619289e78089b7bf04b9e78f85b64fe4ca
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-09 17:38:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,11 @@
-resource "aws_route_table" "public_rt" {
-  vpc_id = data.aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public-rt"
-  }
+variable "aws_region" {
+  default = "us-east-1"
 }
 
-# Associate Public Subnet
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = data.aws_subnet.public.id
-  route_table_id = aws_route_table.public_rt.id
+variable "vpc_name" {
+  default = "OTMS-vpc"
+}
+
+variable "public_subnet_name" {
+  default = "public-subnet"
 }
