2dfcc1facb95c8f869d87fa0418c671110ae0d58
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-09 17:28:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Get existing VPC
+data "aws_vpc" "existing_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+# Get Public Subnet (where NAT will be placed)
+data "aws_subnet" "public_subnet" {
+  filter {
+    name   = "tag:Name"
+    values = [var.public_subnet_name]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.existing_vpc.id]
+  }
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = data.aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [aws_eip.nat_eip]
+}
