e533ba6350d19f1c034b3e92d350decaa890ece6
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-09 17:41:37 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Generate Private Key
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# Create AWS Key Pair using generated public key
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.ssh_key.public_key_openssh
+
+  tags = {
+    Name = var.key_name
+  }
+}
+
+# Save Private Key locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "${var.key_name}.pem"
+}
