531ed935bf048114cd4a057dd97ac2409e7cb853
Abhinav Sikarwar <abhinav.sikarwar@mygurukulam.co>
2026-04-09 16:13:04 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"  # N. Virginia
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/25"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "optimized-vpc"
+}
