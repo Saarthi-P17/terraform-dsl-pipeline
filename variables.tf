db1b331f18b262c134d761bd1a72578d44709f64
mukesh <mukesh@gmail.com>
2026-04-15 15:29:47 +0530

    changed the CIDR to 16 from 25

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/25"
+  default     = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
