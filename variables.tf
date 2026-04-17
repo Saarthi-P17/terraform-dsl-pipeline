3611744375069e1dbcd99c46cc05ab7501255212
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-17 14:43:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "vpc_name" {
   description = "Existing VPC Name"
   type        = string
