3fbe5ad5d8c0ba205ebccf16285e66ccd25941ef
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-17 13:00:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,21 +13,19 @@ variable "vpc_name" {
 # Subnet CIDRs
 // bastion
 variable "public_subnet_cidr" {
-  default = "10.0.0.0/24"
+  default = "10.0.0.0/28"
 }
 //frontend
 variable "private_subnet_1_cidr" {
-  default = "10.0.4.0/23"
+  default = "10.0.0.16/28"
 }
 // backend
 variable "private_subnet_2_cidr" {
-  default = "10.0.8.0/22"
+  default = "10.0.0.32/28"
 }
 // db
 variable "private_subnet_3_cidr" {
-  default = "10.0.12.0/23"
+  default = "10.0.0.48/28"
 }
 
-//variable "private_subnet_4_cidr" {
-//  default = "10.0.0.160/27"
-//}
+
