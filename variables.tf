d1c477d6a5a3e332f7300b30604b48a3f3e8c4dc
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-17 14:43:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "key_name" {
   description = "AWS Key Pair ka naam"
   type        = string
