df150b7b44d3be44f2599e0a80b80be5e8711cb3
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-18 01:34:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,81 +1,60 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 terraform {
   backend "s3" {
+    bucket         = "otms-dev-state"
+    key            = "env/dev/application/network/route-table/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+# ✅ REMOTE STATES
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
     bucket = "otms-dev-state"
-    region = "us-east-1"    # ✅ hardcoded
-    key    = "env/dev/application/network/route-table/terraform.tfstate"
-  }
-}
-# Get existing VPC
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc_name]
+    key    = "env/dev/application/network/vpc/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
-# Get IGW
-data "aws_internet_gateway" "igw" {
-  filter {
-    name   = "attachment.vpc-id"
-    values = [data.aws_vpc.vpc.id]
+data "terraform_remote_state" "subnet" {
+  backend = "s3"
+  config = {
+    bucket = "otms-dev-state"
+    key    = "env/dev/application/network/subnet/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
-# Get NAT Gateway
-data "aws_nat_gateway" "nat" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.vpc.id]
+data "terraform_remote_state" "igw" {
+  backend = "s3"
+  config = {
+    bucket = "otms-dev-state"
+    key    = "env/dev/application/network/IGW/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
-# Get Subnets
-data "aws_subnet" "public" {
-  filter {
-    name   = "tag:Name"
-    values = [var.public_subnet_name]
+data "terraform_remote_state" "nat" {
+  backend = "s3"
+  config = {
+    bucket = "otms-dev-state"
+    key    = "env/dev/application/network/NAT/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
-data "aws_subnet" "private1" {
-  filter {
-    name   = "tag:Name"
-    values = ["private-subnet-1"]
-  }
-}
-
-data "aws_subnet" "private2" {
-  filter {
-    name   = "tag:Name"
-    values = ["private-subnet-2"]
-  }
-}
-
-data "aws_subnet" "private3" {
-  filter {
-    name   = "tag:Name"
-    values = ["private-subnet-3"]
-  }
-}
-/*
-data "aws_subnet" "private4" {
-  filter {
-    name   = "tag:Name"
-    values = ["private-subnet-4"]
-  }
-}
-*/
-
-# ---------------- PUBLIC ROUTE TABLE ----------------
+# PUBLIC ROUTE TABLE
 resource "aws_route_table" "public_rt" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
+    gateway_id = data.terraform_remote_state.igw.outputs.igw_id
   }
 
   tags = {
@@ -83,19 +62,18 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-# Associate Public Subnet
 resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = data.aws_subnet.public.id
+  subnet_id      = data.terraform_remote_state.subnet.outputs.public_subnet_id
   route_table_id = aws_route_table.public_rt.id
 }
 
-# ---------------- PRIVATE ROUTE TABLE ----------------
+# PRIVATE ROUTE TABLE
 resource "aws_route_table" "private_rt" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = data.aws_nat_gateway.nat.id
+    nat_gateway_id = data.terraform_remote_state.nat.outputs.nat_gateway_id
   }
 
   tags = {
@@ -103,25 +81,17 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-# Associate Private Subnets
 resource "aws_route_table_association" "private_assoc_1" {
-  subnet_id      = data.aws_subnet.private1.id
+  subnet_id      = data.terraform_remote_state.subnet.outputs.private_subnet_ids[0]
   route_table_id = aws_route_table.private_rt.id
 }
 
 resource "aws_route_table_association" "private_assoc_2" {
-  subnet_id      = data.aws_subnet.private2.id
+  subnet_id      = data.terraform_remote_state.subnet.outputs.private_subnet_ids[1]
   route_table_id = aws_route_table.private_rt.id
 }
 
 resource "aws_route_table_association" "private_assoc_3" {
-  subnet_id      = data.aws_subnet.private3.id
+  subnet_id      = data.terraform_remote_state.subnet.outputs.private_subnet_ids[2]
   route_table_id = aws_route_table.private_rt.id
 }
-
-/*
-resource "aws_route_table_association" "private_assoc_4" {
-  subnet_id      = data.aws_subnet.private4.id
-  route_table_id = aws_route_table.private_rt.id
-}
-*/
