f08093185f29d0902074da17994b1fa3b1f25100
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-19 01:31:09 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  description = "Environment name"
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Project name"
+  default     = "otms"
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
