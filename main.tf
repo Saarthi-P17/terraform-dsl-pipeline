5a63c2263d964a7ee15352772e18b9eab0f5bc5b
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-19 01:30:33 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+  region = var.region
+}
+
+#  Remote State: VPC
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state"
+    key    = "env/dev/application/network/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+#  Security Group
+resource "aws_security_group" "sg" {
+  name        = "${var.project}-${var.env}-security-group"
+  description = "Security group for OTMS"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  # Inbound Rule 1 (All TCP)
+  ingress {
+    description = "Allow all TCP"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #  Inbound Rule 2 (HTTP)
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #  Outbound (default allow all)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project}-${var.env}-security-group"
+    Environment = var.env
+    Project     = var.project
+  }
+}
