9276d97a7c7623661be1264532111464612388f1
ajitesh70 <132501687+ajitesh70@users.noreply.github.com>
2026-04-19 18:00:14 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "launch_template_id" {
+  value = aws_launch_template.app_template.id
+}
+
+output "launch_template_latest_version" {
+  value = aws_launch_template.app_template.latest_version
+}
