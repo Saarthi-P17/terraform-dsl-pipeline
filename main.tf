626dfefc37383a7d0921682b05a5b1784495b39e
ajitesh70 <132501687+ajitesh70@users.noreply.github.com>
2026-04-19 17:59:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  backend "s3" {
+    bucket         = "otms-dev-state"
+    key            = "env/dev/application/otms/ec2-template/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# 🔹 Subnet Remote State
+data "terraform_remote_state" "subnet" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state"
+    key    = "env/dev/application/network/subnet/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# 🔹 External ALB SG Remote State
+data "terraform_remote_state" "alb_sg" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state"
+    key    = "env/dev/application/otms/external-alb/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# 🔹 Launch Template
+resource "aws_launch_template" "app_template" {
+  name_prefix   = "${var.project}-${var.env}-template"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [
+    data.terraform_remote_state.alb_sg.outputs.security_group_id
+  ]
+
+  # Optional but useful
+  monitoring {
+    enabled = true
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name        = "${var.project}-${var.env}-app"
+      Environment = var.env
+      Project     = var.project
+    }
+  }
+}
