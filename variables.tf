d43210e29d9a6f0527520456ffecaa42f8fa91ac
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-19 17:45:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "env" {
+  default = "dev"
+}
+
+variable "project" {
+  default = "otms"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-033bd18826652770f"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
