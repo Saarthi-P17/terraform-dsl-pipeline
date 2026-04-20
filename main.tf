55b97e0ac6e02a88acc5dd1c1dfb3f4d3a720c55
mukesh <mukesh@gmail.com>
2026-04-20 23:23:23 +0530

    need to add network eni

--- main.tf
+++ main.tf
@@ -10,24 +10,45 @@ terraform {
 provider "aws" {
   region = var.region
 }
-/*
+
 # 🔹 VPC Remote State
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket = "otms-dev-state"
+    bucket = "otms-dev-state7864582"
     key    = "env/dev/application/network/vpc/terraform.tfstate"
     region = "us-east-1"
   }
 }
+// bastion security grpo remote state
+data "terraform_remote_state" "bastion_sg" {
+  backend = "s3"
 
+  config = {
+    bucket = "otms-dev-state7864582"
+    key    = "env/dev/backend/security-group/bastion/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+//fetching the backend state file of otms backend
+data "terraform_remote_state" "ec2" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state7864582"
+    key    = "env/dev/backend/instances/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+/*
 # 🔹 External ALB SG Remote State
 data "terraform_remote_state" "alb_sg" {
   backend = "s3"
 
   config = {
-    bucket = "otms-dev-state"
+    bucket = "otms-dev-state7864582"
     key    = "env/dev/application/otms/external-alb/terraform.tfstate"
     region = "us-east-1"
   }
@@ -42,13 +63,17 @@ resource "aws_security_group" "notification_sg" {
 
   #  PORT 5000
   ingress {
-    from_port                = 5000
-    to_port                  = 5000
-    protocol                 = "tcp"
-    source_security_group_id = "env/dev/backend/security-group/bastion/terraform.tfstate"
-  }
+  from_port = 5000
+  to_port   = 5000
+  protocol  = "tcp"
 
-  # ✅ SSH (optional but as per your screenshot)
+  security_groups = [
+    data.terraform_remote_state.bastion_sg.outputs.bastion_sg_id,
+    //data.terraform_remote_state.alb_sg.outputs.id
+  ]
+}
+
+  #  SSH (optional but as per your screenshot)  
   ingress {
     from_port   = 22
     to_port     = 22
@@ -56,7 +81,7 @@ resource "aws_security_group" "notification_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # ✅ OUTBOUND ALL
+  #  OUTBOUND ALL
   egress {
     from_port   = 0
     to_port     = 0
@@ -69,4 +94,13 @@ resource "aws_security_group" "notification_sg" {
     Environment = var.env
     Project     = var.project
   }
+}
+output "notification_sg" {
+  value = aws_security_group.notification_sg.id
+}
+
+//saecurity group attachment to ec2
+resource "aws_network_interface_sg_attachment" "attach_notification_sg" {
+  security_group_id    = aws_security_group.notification_sg.id
+  network_interface_id = data.terraform_remote_state.ec2.outputs.primary_network_interface_id
 }
