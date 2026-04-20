0f12e0e34e9e9aa3b0614fb04f186c5c7a53ebf1
mukesh <mukesh@gmail.com>
2026-04-20 22:34:04 +0530

    no changes so far

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "otms-dev-state"
-    key            = "env/dev/application/otms/notification-sg/terraform.tfstate"
+    bucket         = "otms-dev-state7864582"
+    key            = "env/dev/backend/notification-sg/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
+/*
 # 🔹 VPC Remote State
 data "terraform_remote_state" "vpc" {
   backend = "s3"
@@ -32,43 +32,20 @@ data "terraform_remote_state" "alb_sg" {
     region = "us-east-1"
   }
 }
-
+*/
 # 🔹 Notification Security Group
 resource "aws_security_group" "notification_sg" {
   name        = "${var.project}-${var.env}-notification-sg"
   description = "Notification Security Group"
   vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  # ✅ PORT 8080
-  ingress {
-    from_port                = 8080
-    to_port                  = 8080
-    protocol                 = "tcp"
-    source_security_group_id = data.terraform_remote_state.alb_sg.outputs.security_group_id
-  }
 
-  # ✅ PORT 8081
-  ingress {
-    from_port                = 8081
-    to_port                  = 8081
-    protocol                 = "tcp"
-    source_security_group_id = data.terraform_remote_state.alb_sg.outputs.security_group_id
-  }
-
-  # ✅ PORT 8082
-  ingress {
-    from_port                = 8082
-    to_port                  = 8082
-    protocol                 = "tcp"
-    source_security_group_id = data.terraform_remote_state.alb_sg.outputs.security_group_id
-  }
-
-  # ✅ PORT 5000
+  #  PORT 5000
   ingress {
     from_port                = 5000
     to_port                  = 5000
     protocol                 = "tcp"
-    source_security_group_id = data.terraform_remote_state.alb_sg.outputs.security_group_id
+    source_security_group_id = "env/dev/backend/security-group/bastion/terraform.tfstate"
   }
 
   # ✅ SSH (optional but as per your screenshot)
