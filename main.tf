95b45ebdea5b8fb0e84ef9afdc79171c05bdd28f
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-20 14:00:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "otms-dev-state"
+    bucket         = "otms-dev-state7864582"
     key            = "env/dev/application/network/NAT/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
@@ -15,7 +15,7 @@ terraform {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "otms-dev-state"
+    bucket = "otms-dev-state7864582"
     key    = "env/dev/application/network/vpc/terraform.tfstate"
     region = "us-east-1"
   }
@@ -25,7 +25,7 @@ data "terraform_remote_state" "vpc" {
 data "terraform_remote_state" "subnet" {
   backend = "s3"
   config = {
-    bucket = "otms-dev-state"
+    bucket = "otms-dev-state7864582"
     key    = "env/dev/application/network/subnet/terraform.tfstate"
     region = "us-east-1"
   }
