9724aa3542deeacaca7b730aab8d76e2e8150eff
Hardik Modi <126283577+hardbro786@users.noreply.github.com>
2026-04-20 15:53:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,16 @@
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+# Public Subnets
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_subnet_1a.id,
+    aws_subnet.public_subnet_1b.id
+  ]
 }
 
+# Private Subnets
 output "private_subnet_ids" {
   value = [
     aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id,
-    aws_subnet.private_subnet_3.id,
-    //aws_subnet.private_subnet_4.id
+    aws_subnet.private_subnet_3.id
   ]
 }
