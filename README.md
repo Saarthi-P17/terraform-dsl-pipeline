53d877ace0d71228e807d39480edf4a3e5b5ed58
mukesh <mukesh@gmail.com>
2026-04-20 22:56:05 +0530

    created bastion security group and bastion host in this branch. fallback v1

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # terraform
 terraform
+
+created a bastion host and a bastion security group attached to it
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state7864582"
+    key    = "env/dev/application/network/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+# subnet block
+data "terraform_remote_state" "subnets" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state7864582"
+    key    = "env/dev/application/network/subnet/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+# bsation id
+output "bastion_sg_id" {
+  description = "Security Group ID for Bastion host"
+  value       = aws_security_group.bastion_sg.id
+}
--- main.tf
+++ main.tf
@@ -1,10 +1,28 @@
 # ---------------------------
 # Bastion Security Group
 # ---------------------------
+terraform {
+  backend "s3" {
+    bucket         = "otms-dev-state7864582"
+    key            = "env/dev/backend/security-group/bastion/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+// fetching vpc id
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "otms-dev-state7864582"
+    key    = "env/dev/application/network/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion-sg"
   description = "Allow SSH access to bastion host"
-  vpc_id      = "env/dev/application/network/subnet/terraform.tfstate"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
   ingress {
     description = "SSH from trusted IP"
@@ -42,4 +60,48 @@ data "terraform_remote_state" "subnets" {
 output "bastion_sg_id" {
   description = "Security Group ID for Bastion host"
   value       = aws_security_group.bastion_sg.id
+}
+
+variable "env" {
+  description = "Environment name (dev/stage/prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "Environment name (dev/stage/prod)"
+  type        = string
+  default     = "OTMS"
+}
+
+output "bastion_instance_details" {
+  value = {
+    instance_id = aws_instance.bastion.id
+    subnet_id   = aws_instance.bastion.subnet_id
+    sg_id       = aws_security_group.bastion_sg.id
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami           = "ami-04680790a315cd58d"
+  instance_type = "t3.micro"
+
+  #  Attach to PUBLIC subnet
+  subnet_id = data.terraform_remote_state.subnets.outputs.public_subnet_ids[0]
+
+  #  Attach Security Group
+  vpc_security_group_ids = [
+    aws_security_group.bastion_sg.id
+  ]
+
+  key_name = "my-terraform-key"
+
+  tags = {
+    Name    = "bastion-host"
+    env     = var.env
+    project = var.project
+  }
+}
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
 }
